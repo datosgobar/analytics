7458f4024a844ba7bc6e2ebdba8cd2e9646d199f
Agustín Benassi <agusbenassi@gmail.com>
2019-11-28 18:17:22 -0300

    Cambio nombre del path credenciales.

--- tools/analytics_dump.py
+++ tools/analytics_dump.py
@@ -66,7 +66,7 @@ def create_analytics_zip(analytics_csv_pattern, analytics_zip,
 
 def main():
     REPO_NAME = "analytics"
-    REPO_DIR = os.path.abspath('./').split(REPO_NAME)[0] + REPO_NAME
+    REPO_DIR = os.path.abspath(os.path.dirname(__file__))
     CREDENTIALS_FILE = '/config.json'
     CREDENTIALS_PATH = REPO_DIR + CREDENTIALS_FILE
     config = json.load(open(CREDENTIALS_PATH))
